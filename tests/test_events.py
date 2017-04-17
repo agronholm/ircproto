c1665e05aad3541ffe0f41b2ce51050e408b3a3b
Benno Rice <benno@jeamland.net>
2017-04-17 11:22:16 -0700

    Add a test for the fallback decode path in decode_event.

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,3 +1,5 @@
+import codecs
+
 import pytest
 
 from ircproto.events import decode_event
@@ -14,3 +16,10 @@ def test_decode_event_unknown_command():
     buffer = bytearray(b':foo!bar@blah FROBNICATE\r\n')
     exc = pytest.raises(UnknownCommand, decode_event, buffer)
     assert str(exc.value) == 'IRC protocol violation: unknown command: FROBNICATE'
+
+
+def test_decode_fallback_conversion():
+    decoder = codecs.getdecoder('utf-8')
+    fallback_decoder = codecs.getdecoder('iso-8859-1')
+    buffer = bytearray(b':foo!bar@blah PRIVMSG hey du\xe2\xa9k\r\n')
+    assert decode_event(buffer, decoder=decoder, fallback_decoder=fallback_decoder)
